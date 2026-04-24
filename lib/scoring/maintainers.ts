79e02b23f8427a7ed2fd0330c2ee4637c204c5fd
AnasSarkiz <anassoftwaredev@gmail.com>
2026-04-24 20:52:46 +0200

    upgrade Anas to maintainer 4

--- lib/scoring/maintainers.ts
+++ lib/scoring/maintainers.ts
@@ -2,7 +2,7 @@ export const MAINTAINERS = {
   ShiboSoftwareDev: "maintainer5",
   "ArnavK-09": "maintainer1",
   Abse2001: "maintainer4",
-  AnasSarkiz: "maintainer3",
+  AnasSarkiz: "maintainer4",
 }
 
 // Alum Maintainers
